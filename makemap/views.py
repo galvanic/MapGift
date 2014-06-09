6c610dfbd34820b84492af16c2f1bfe29314458f
Galvanic <justinecourty@yahoo.fr>
2014-06-09 15:07:37 +0100

    Add index page and map views to urls.

--- makemap/views.py
+++ makemap/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+import mapgift
+from makemap.models import Map, KMLfile
+
 # Create your views here.
+
+def index(request):
+    context = {"providers": mapgift.PROVIDERS,}
+    return render(request, 'makemap/index.html', context)
