7ccdbc87717236317cfdc1d2ed72ed9fad943015
Galvanic <justinecourty@yahoo.fr>
2014-06-10 09:18:55 +0100

    Change settings for deployment to heroku.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn maps_project.wsgi
--- maps_project/settings.py
+++ maps_project/settings.py
@@ -89,12 +89,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 ## for KML files storage and other media
 
 MEDIA_ROOT = "/media/"
 
+## Added for heroku
+# Parse database configuration from $DATABASE_URL
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
